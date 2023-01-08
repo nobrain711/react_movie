5820d18370e1ae6f2f6ec745fc0bbde651561da2
nobrain711 <qa9975@ync.ac.kr>
2023-01-09 08:17:51 +0900

    Updates

---
+++ precache-manifest.aca4816a03e5ade95044ae921d89a5de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3db43e9fc67c58d1b694",
+    "url": "/react_movie/static/css/main.440620bd.chunk.css"
+  },
+  {
+    "revision": "3db43e9fc67c58d1b694",
+    "url": "/react_movie/static/js/main.3db43e9f.chunk.js"
+  },
+  {
+    "revision": "5df50f20135844d1f62b",
+    "url": "/react_movie/static/js/1.5df50f20.chunk.js"
+  },
+  {
+    "revision": "151eab3afe1df027587a",
+    "url": "/react_movie/static/js/2.151eab3a.chunk.js"
+  },
+  {
+    "revision": "68ad10b1827dd725a819",
+    "url": "/react_movie/static/js/runtime~main.68ad10b1.js"
+  },
+  {
+    "revision": "7c22ea02f11ed83fdbebaa8823d693d0",
+    "url": "/react_movie/index.html"
+  }
+];
