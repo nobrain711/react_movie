117d34cf0b6e564d9013e2cd5d02cc0e16fecce4
nobrain711 <qa9975@ync.ac.kr>
2023-03-29 16:18:35 +0900

    영화 정보 페이지 배경이미지 적용완료

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -11,7 +12,29 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      console.log(location.state);
+      return (
+        <div
+          className="movie_detail"
+          style={{
+            backgroundImage: `url(${location.state.poster})`,
+            backgroundRepeat: `no-repeat`,
+            backgroundSize: `cover`,
+            backgroundAttachment: `fixed`,
+          }}
+        >
+          <span>{location.state.year}</span>
+          <br />
+          <span>{location.state.title}</span>
+          <br />
+          <span>{location.state.summary}</span>
+          <br />
+          <span>{location.state.poster}</span>
+          <br />
+          <span>{location.state.genegers}</span>
+          <br />
+        </div>
+      );
     } else {
       return null;
     }
